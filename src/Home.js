e61ed32834b9b881f51835e28a5a5b9f1471aa12
Joana Carballo <joana16cruz@gmail.com>
2022-02-25 12:04:08 +0100

    iteration 1

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+
+const Home = props => {
+    
+    
+
+    return (
+        <div>
+            <div>
+                <p><Link to='/beers'>All Beers</Link></p>
+                <p><Link to='/random-beer'>Random Beer</Link></p>
+                <p><Link to='/new-beer'>New Beer</Link></p>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Home;
